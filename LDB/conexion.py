19f5f1c74bad0515c54511b1fbf4ab34095536be
David Leal <davinchi.leal123@gmail.com>
2023-10-10 23:05:17 -0500

    update

--- LDB/conexion.py
+++ LDB/conexion.py
@@ -44,4 +44,31 @@ class DAO():
                 self.conexion.commit()
                 return 'Producto "{0} registrado"'.format(producto.get('nombrec'))
             except Error as ex:
-                print('Error al intentar la conexion: {0}'.format(ex))                
+                print('Error al intentar la conexion: {0}'.format(ex))
+
+    def actualizar_producto(self, producto):
+        if self.conexion.is_connected():
+            try:
+                cursor = self.conexion.cursor()
+                cursor.execute("UPDATE producto SET {0} = {1}, {2} = '{3}', {4} = '{5}', {6} = '{7}', {8} = '{9}', {10} = '{11}', {12} = '{13}', {14} = '{15}')".format(
+                    'producto_nombrecorto'
+                    , producto.get('nombrec') if producto.get('nombrec') is not None else ""
+                    ,'producto_nombrelargo'
+                    , producto.get('nombrel') if producto.get('nombrel') is not None else ""
+                    ,'producto_descripcioncorto'
+                    , producto.get('descc') if producto.get('descc') is not None else ""
+                    , 'producto_descripcionlarga'
+                    , producto.get('descl') if producto.get('descl') is not None else ""
+                    , 'producto_precio'
+                    , producto.get('precio') if producto.get('precio') is not None else ""
+                    , 'producto_promocion'
+                    , producto.get('promocion') if producto.get('promocion') is not None else ""
+                    , 'producto_cantidad'
+                    , producto.get('cantidad') if producto.get('cantidad') is not None else ""
+                    , 'producto_ulrimagen'
+                    , producto.get('url') if producto.get('url') is not None else ""
+                ))
+                self.conexion.commit()
+                return 'Producto "{0} actualizado"'.format(producto.get('nombrec'))
+            except Error as ex:
+                print('Error al intentar la conexion: {0}'.format(ex))  
